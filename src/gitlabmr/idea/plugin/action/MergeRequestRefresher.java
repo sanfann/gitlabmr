a0c12fac2b3191de3fee1d12490e055575100f6a
kurt <kurt@vpon.com>
2015-12-05 15:06:19 +0800

    set icon for refresh action

--- src/gitlabmr/idea/plugin/action/MergeRequestRefresher.java
+++ src/gitlabmr/idea/plugin/action/MergeRequestRefresher.java
@@ -1,9 +1,13 @@
 package gitlabmr.idea.plugin.action;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class MergeRequestRefresher extends AnAction {
+    public MergeRequestRefresher() {
+        super("Refresh", "Refresh merge request list", AllIcons.Actions.Refresh);
+    }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
